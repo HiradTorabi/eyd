d41c72705a304ebee1aa26390711ad03984d6015
HiradTorabi <babak.torabi.1384@gmail.com>
2025-04-03 22:53:37 +0330

    almost done

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,29 @@
 plugins {
-    id("java")
+    kotlin("jvm") version "1.9.10"
+    application
+    id("org.openjfx.javafxplugin") version "0.0.13"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.hirad"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(kotlin("stdlib"))
 }
 
-tasks.test {
-    useJUnitPlatform()
+javafx {
+    version = "17"
+    modules = listOf("javafx.controls", "javafx.fxml")
+}
+
+application {
+    mainClass.set("com.hirad.genus.Main")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "17"
 }
